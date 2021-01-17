9c9e827dc069f225b7ffb652f76ff204d837d0b6
Matthijs Hovestad <matthijs.hovestad.2@politie.nl>
2021-01-17 21:28:17 +0100

    starting menu

--- ApplicationHexapod.py
+++ ApplicationHexapod.py
@@ -6,3 +6,4 @@ if __name__ == '__main__':
     logging.basicConfig(filename='logging.log', level=logging.INFO, format='%(asctime)s %(message)s')
     logging.info("Starting application. Saving logs in ~/logging.log")
     menu = Menu()
+    menu.starting_menu()
